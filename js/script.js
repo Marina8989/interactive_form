8f1dca24f1309e0b341f2fb974c106820a71abd6
Marina8989 <marina.mill209@gmail.com>
2020-06-29 15:13:14 -0700

    working on the first tasks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+//auto focus the first input field when page loaded
 const inputFocus = document.querySelector('#name').focus();
 
+const secOther = document.querySelector('#title');
+const otherTitle = document.querySelector('#other-title');
+
+otherTitle.style.display = 'none';
+//display input if "other" chosen
+secOther.addEventListener('mouseout', function(e) {
+      if(e.target.value === 'other') {
+         otherTitle.style.display = 'block';
+      }else {
+        otherTitle.style.display = 'none';
+    }
+})
+
+//t-shirt info section
+const color = document.querySelector('#color');
+const puns = document.querySelector("[value='js puns']");
+const heart = document.querySelector("[value='heart js']");
+const design = document.querySelector('#design');
+const h3 = document.querySelector('h3');
+
+color.style.display = 'none';
+
+design.addEventListener('click', function(e) {
+    if(e.target.value === 'js puns'){
+         h3.style.display = 'none';
+    }if(e.target.value === 'heart js') {
+        console.log('two');
+    }
+});
 
