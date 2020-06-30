e1aeb67b20922823f9addbdc429c0da23aedc674
Marina8989 <marina.mill209@gmail.com>
2020-06-29 20:42:00 -0700

    added the register for activitied section

--- js/script.js
+++ js/script.js
@@ -17,18 +17,14 @@ secOther.addEventListener('mouseout', function(e) {
 //t-shirt info section
 const color = document.querySelector('#color');
 const colorOption = document.querySelectorAll('#color option');
-console.log(colorOption);
-const puns = document.querySelector("[value='js puns']");
-const heart = document.querySelector("[value='heart js']");
 const design = document.querySelector('#design');
 const h3 = document.querySelector('h3');
-
+//hide the select options
 color.style.display = 'none';
 
-
 design.addEventListener('mouseout', function(e) {
+    h3.style.display = "none";
     if(e.target.value === 'js puns'){
-         h3.style.display = 'none';
          color.style.display = "block";
    
         for (let i = 0; i < colorOption.length; i++) {
@@ -39,12 +35,10 @@ design.addEventListener('mouseout', function(e) {
              colorOption[i].value === "gold"
            ) {
              colorOption[i].style.display = "block";
-           }
-           
+           } 
         }
     }
     if(e.target.value === 'heart js') {
-         h3.style.display = 'none';
          color.style.display = "block";
    
         for (let i = 0; i < colorOption.length; i++) {
@@ -56,7 +50,42 @@ design.addEventListener('mouseout', function(e) {
            ) {
              colorOption[i].style.display = "block";
            }
+        }
     }
-  }
 });
 
+//register for activities
+const activities = document.querySelector('.activities');
+const checkbox = activities.querySelectorAll("input[type='checkbox']");
+
+
+activities.addEventListener('change', function() {
+    if (checkbox[1].checked) {
+      checkbox[3].parentNode.style.color = "rgba(0,0,0,.8)";
+      checkbox[3].disabled = true;
+    }else {
+        checkbox[3].parentNode.style.color = "rgba(0,0,0, 1)";
+        checkbox[3].disabled = false;
+    }
+    if (checkbox[3].checked) {
+      checkbox[1].style.color = "rgba(0,0,0,.8)";
+      checkbox[1].disabled = true;
+    } else {
+      checkbox[1].parentNode.style.color = "rgba(0,0,0, 1)";
+      checkbox[1].disabled = false;
+    }
+    if (checkbox[2].checked) {
+      checkbox[4].parentNode.style.color = "rgba(0,0,0,.8)";
+      checkbox[4].disabled = true;
+    } else {
+      checkbox[4].parentNode.style.color = "rgba(0,0,0, 1)";
+      checkbox[4].disabled = false;
+    }
+    if (checkbox[4].checked) {
+      checkbox[2].style.color = "rgba(0,0,0,.8)";
+      checkbox[2].disabled = true;
+    } else {
+      checkbox[2].parentNode.style.color = "rgba(0,0,0, 1)";
+      checkbox[2].disabled = false;
+    }
+});
